ca89ff9ccd414f9227cfd5eac34904798be8ced4
Ricardo Pedrosa <rickpedrosa94@gmail.com>
2020-05-09 18:18:12 +0200

    NUEVAS TABLAS AÑADIDAS

--- src/main/sql/create/create_table_players.sql
+++ src/main/sql/create/create_table_players.sql
@@ -1,16 +1,16 @@
 create table if not exists players
 (
-  id          int unsigned              not null
-    primary key,
-  name        varchar(100) charset utf8 not null,
-  image       varchar(255) charset utf8 not null,
-  nationality varchar(100) charset utf8 not null,
-  potential   int unsigned              not null,
-  age         int unsigned              not null,
-  club        varchar(100) charset utf8 not null,
-  value       bigint                    null,
-  wage        bigint                    not null,
-  INDEX player_club (club),
-  foreign key (club) references teams (name)
-    on update cascade on delete CASCADE
+    id          int unsigned              not null
+        primary key,
+    name        varchar(100) charset utf8 not null,
+    image       varchar(255) charset utf8 not null,
+    nationality varchar(100) charset utf8 not null,
+    potential   int unsigned              not null,
+    age         int unsigned              not null,
+    club        varchar(100) charset utf8 not null,
+    value       bigint                    null,
+    wage        bigint                    not null,
+    INDEX player_club (club),
+    foreign key (club) references teams (name)
+        on update cascade on delete CASCADE
 )
